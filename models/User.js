978a7c0208a35f8cbd0e43cab8f8577475510448
Franco21198 <francoromanazzi@hotmail.com>
2018-12-28 22:22:37 -0300

    User api routes

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const timestamp = require('mongoose-timestamp');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.plugin(timestamp);
+
+const Customer = mongoose.model('users', UserSchema);
+module.exports = Customer;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const playlists = require('./routes/api/playlists');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -18,11 +25,25 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
 app.use('/api/playlists', playlists);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
